4cec1521db1dd0d46caab50f59e8d36281242957
Andre Ricardo <andrericardo.developer@outlook.com>
2025-01-15 17:26:13 -0300

    move NextEventPlayer to its own file

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_advanced_manguinho/domain/entities/next_event_player.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -41,48 +42,3 @@ void main() {
   });
 }
 
-class NextEventPlayer {
-  final String id;
-  final String name;
-  final String initials;
-  final String? photo;
-  final String? position;
-  final bool isConfirmed;
-  final DateTime? confirmationDate;
-
-  NextEventPlayer._({
-    required this.id,
-    required this.name,
-    required this.initials,
-    this.photo,
-    this.position,
-    required this.isConfirmed,
-    this.confirmationDate,
-  });
-
-  factory NextEventPlayer({
-    required String id,
-    required String name,
-    String? photo,
-    String? position,
-    required bool isConfirmed,
-    DateTime? confirmationDate,
-  }) =>
-      NextEventPlayer._(
-        id: id,
-        name: name,
-        initials: _getInitials(name),
-        photo: photo,
-        position: position,
-        isConfirmed: isConfirmed,
-        confirmationDate: confirmationDate,
-      );
-
-  static String _getInitials(String name) {
-    final names = name.toUpperCase().trim().split(' ');
-    String firstChar = names.first.split('').firstOrNull ?? '-';
-    String lastChar =
-        names.last.split('').elementAtOrNull(names.length == 1 ? 1 : 0) ?? '';
-    return '$firstChar$lastChar';
-  }
-}
