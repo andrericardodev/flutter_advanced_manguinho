1797742c686a9f0f0a7e5c3c387bd6cab43aa41d
Andre Ricardo <andrericardo.developer@outlook.com>
2025-01-15 16:14:11 -0300

    change strategy to use factory constructor

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -13,24 +13,41 @@ void main() {
 class NextEventPlayer {
   final String id;
   final String name;
-  late final String initials;
+  final String initials;
   final String? photo;
   final String? position;
   final bool isConfirmed;
   final DateTime? confirmationDate;
 
-  NextEventPlayer({
+  NextEventPlayer._({
     required this.id,
     required this.name,
+    required this.initials,
     this.photo,
     this.position,
     required this.isConfirmed,
     this.confirmationDate,
-  }) {
-    initials = _getInitials();
-  }
+  });
 
-  String _getInitials() {
+  factory NextEventPlayer({
+    required String id,
+    required String name,
+    String? photo,
+    String? position,
+    required bool isConfirmed,
+    DateTime? confirmationDate,
+  }) =>
+      NextEventPlayer._(
+        id: id,
+        name: name,
+        initials: _getInitials(name),
+        photo: photo,
+        position: position,
+        isConfirmed: isConfirmed,
+        confirmationDate: confirmationDate,
+      );
+
+  static String _getInitials(String name) {
     final names = name.split(' ');
     String firstChar = names.first[0];
     String lastChar = names.last[0];
